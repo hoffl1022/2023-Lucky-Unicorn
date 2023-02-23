6de9b0f0237e0d0db46dcf292055a408c903a8b8
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2023-02-23 14:27:49 +1300

    Add files via upload

--- 01_sandpit.py
+++ 01_sandpit.py
@@ -1,1 +1,10 @@
-print("hello world")
+# Ask users for a number
+get_number = int(input("choose a number "))
+
+# multiply number by five
+times_five = get_number * 5
+
+answer = "{} times 5 is equal to {}".format(get_number, times_five)
+
+# Output the result
+print(answer)
