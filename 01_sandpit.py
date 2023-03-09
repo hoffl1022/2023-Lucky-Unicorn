f0b3c91158c0be9efaeca04a564e7da8dc6efb49
hoffl1022 <113646106+hoffl1022@users.noreply.github.com>
2023-03-09 14:44:43 +1300

    Add files via upload

--- 01_sandpit.py
+++ 01_sandpit.py
@@ -1,5 +1,6 @@
-import random
 
-for item in range(0, 500):
-    number = random.randint(1, 10)
-    print(number)
+while True:
+    number = int(input("choose a number "))
+
+    final_number = number ** number ** number
+    print(final_number)
